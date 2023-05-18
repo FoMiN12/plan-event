d23289aafd683f84cce859bab98ece24ad269480
Kirill Fomin <fomin102011@yandex.ru>
2023-05-18 19:06:06 +0300

    DateConverter written

---
+++ app/src/main/java/com/example/planevent/models/converters/DateConverter.java
@@ -0,0 +1,25 @@
+package com.example.planevent.models.converters;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+public class DateConverter {
+    @TypeConverter
+    public static Date fromTimestamp(Long value) {
+        if (value == null) {
+            return null;
+        } else {
+            return new Date(value);
+        }
+    }
+
+    @TypeConverter
+    public static Long dateToTimestamp(Date date) {
+        if (date == null) {
+            return null;
+        } else {
+            return date.getTime();
+        }
+    }
+}
