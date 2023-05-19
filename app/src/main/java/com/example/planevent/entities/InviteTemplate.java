ac5eb49da82d94dc3c6930ab1e5007f965b1c747
Kirill Fomin <fomin102011@yandex.ru>
2023-05-19 03:49:44 +0300

    More constructors for InviteTemplate

--- app/src/main/java/com/example/planevent/entities/InviteTemplate.java
+++ app/src/main/java/com/example/planevent/entities/InviteTemplate.java
@@ -3,6 +3,7 @@ package com.example.planevent.entities;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 
 import com.example.planevent.entities.parent.classes.EventElement;
 import com.example.planevent.util.Gender;
@@ -15,6 +16,19 @@ public class InviteTemplate extends EventElement {
     public InviteTemplate(int eventId, @NonNull String name, String description, Gender targetGender) {
         super(eventId, name, description);
         this.targetGender = targetGender;
+    }
+
+    @Ignore
+    public InviteTemplate(int eventId, @NonNull String name) {
+        this(eventId, name, "", null);
+    }
+    @Ignore
+    public InviteTemplate(int eventId, @NonNull String name, Gender targetGender) {
+        this(eventId, name, "", targetGender);
+    }
+    @Ignore
+    public InviteTemplate(int eventId, @NonNull String name, String description) {
+        this(eventId, name, description, null);
     }
 
 
